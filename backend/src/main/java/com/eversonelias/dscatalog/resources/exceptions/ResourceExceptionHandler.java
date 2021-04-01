259848d9d3bcb7e8a3fa5530b7f9c1986985231e
Everson Elias <eversoneliasg@gmail.com>
2021-04-01 09:57:29 -0300

    Update category

--- backend/src/main/java/com/eversonelias/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/eversonelias/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.eversonelias.dscatalog.services.exceptions.EntityNotFoundException;
+import com.eversonelias.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNoFound(EntityNotFoundException e, HttpServletRequest request) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNoFound(ResourceNotFoundException e, HttpServletRequest request) {
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(HttpStatus.NOT_FOUND.value());
