7162d409724f64a54c9551fbb4df2c2f47d977f9
Everson Elias <eversoneliasg@gmail.com>
2021-04-01 10:20:42 -0300

    Delete category

--- backend/src/main/java/com/eversonelias/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/eversonelias/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.eversonelias.dscatalog.dto.CategoryDTO;
 import com.eversonelias.dscatalog.entities.Category;
 import com.eversonelias.dscatalog.repositories.CategoryRepository;
+import com.eversonelias.dscatalog.services.exceptions.DatabaseException;
 import com.eversonelias.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service // It will register my class as a component of the dependency injection automated system.
@@ -52,6 +55,18 @@ public class CategoryService {
 		}
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }
